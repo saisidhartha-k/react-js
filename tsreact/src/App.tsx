d90ad5d19fbf70d4e49919e88091b9bb954738a9
saisidharth-k <saisidhartha.k@prodapt.com>
2023-10-07 15:07:04 +0530

    ic

---
+++ tsreact/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+type x = {
+  c : string;
+}
+
+function App() {
+  return (
+   <>
+   hello</>
+  );
+}
+
+export default App;
